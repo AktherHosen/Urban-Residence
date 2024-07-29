981c5f8dd9618422bf98c4b702b36d1414b419b8
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-07-29 16:15:56 +0600

    added login and registration form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,68 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { IoEye } from "react-icons/io5";
+import { IoMdEyeOff } from "react-icons/io";
+import { Link } from "react-router-dom";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="p-4">
+      <h1 className="text-center my-4 text-xl font-exo text-vsecondary font-semibold">
+        Please Login
+      </h1>
+      <div className="flex justify-center">
+        <div className="w-full md:w-[450px]  flex flex-col justify-center border shadow-sm p-4">
+          <form action="" className="">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              className="block border rounded-sm px-3 py-2 my-1 w-full"
+              placeholder="Enter your email"
+            />
+            <div className="relative">
+              <label htmlFor="password">Password</label>
+              <br />
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                className="border rounded-sm px-3 py-2 my-1 w-full"
+                placeholder="Enter your password"
+              />
+              <span
+                className="absolute right-3 top-[65%] transform -translate-y-1/2 cursor-pointer"
+                onClick={togglePasswordVisibility}
+              >
+                {showPassword ? <IoMdEyeOff /> : <IoEye />}
+              </span>
+            </div>
+            <button className="w-full px-3 py-2 rounded-sm bg-black font-semibold text-white my-2 cursor-pointer">
+              Login
+            </button>
+            <h3>
+              New here? Please
+              <Link to="/register" className="text-vsecondary hover:underline">
+                <span> </span> Register
+              </Link>
+            </h3>
+          </form>
+          <div className="mt-2">
+            <p className="text-center font-exo">Sign in with</p>
+            <div className="flex justify-center gap-4">
+              <button className="border bg-orange-500 text-white rounded-sm p-1">
+                <FaGoogle size={25} />
+              </button>
+              <button className="border bg-black text-white rounded-sm p-1">
+                <FaGithub size={25} />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { IoEye } from "react-icons/io5";
 import { IoMdEyeOff } from "react-icons/io";
+import { Link } from "react-router-dom";
 const Register = () => {
   const [showPassword, setShowPassword] = useState(false);
 
@@ -12,50 +13,57 @@ const Register = () => {
       <h1 className="text-center my-4 text-xl font-exo text-vsecondary font-semibold">
         Please Register
       </h1>
-      <form action="" className="w-[300px]">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          className="block border rounded-sm px-3 py-2 my-1"
-          placeholder="Enter your name"
-        />
-
-        <label htmlFor="photo">Photo Url</label>
-        <input
-          type="text"
-          name="photo"
-          className="block border rounded-sm px-3 py-2 my-1"
-          placeholder="Enter your photo url"
-        />
-
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          className="block border rounded-sm px-3 py-2 my-1"
-          placeholder="Enter your email"
-        />
-
-        <div className="relative">
-          <label htmlFor="password">Password</label>
-          <br />
+      <div className="flex justify-center">
+        <form action="" className="w-full md:w-[450px] border shadow-sm p-4">
+          <label htmlFor="name">Name</label>
           <input
-            type={showPassword ? "text" : "password"}
-            name="password"
-            className="border rounded-sm px-3 py-2 my-1 w-full"
-            placeholder="Enter your password"
+            type="text"
+            name="name"
+            className="block border rounded-sm px-3 py-2 my-1 w-full"
+            placeholder="Enter your name"
           />
+          <label htmlFor="photo">Photo Url</label>
+          <input
+            type="text"
+            name="photo"
+            className="block border rounded-sm px-3 py-2 my-1 w-full"
+            placeholder="Enter your photo url"
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            className="block border rounded-sm px-3 py-2 my-1 w-full"
+            placeholder="Enter your email"
+          />
+          <div className="relative">
+            <label htmlFor="password">Password</label>
+            <br />
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              className="border rounded-sm px-3 py-2 my-1 w-full"
+              placeholder="Enter your password"
+            />
 
-          {/* Adjusted positioning for the eye icon */}
-          <span
-            className="absolute right-3 top-[65%] transform -translate-y-1/2 cursor-pointer"
-            onClick={togglePasswordVisibility}
-          >
-            {showPassword ? <IoMdEyeOff /> : <IoEye />}
-          </span>
-        </div>
-      </form>
+            <span
+              className="absolute right-3 top-[65%] transform -translate-y-1/2 cursor-pointer"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? <IoMdEyeOff /> : <IoEye />}
+            </span>
+          </div>
+          <button className="w-full px-3 py-2 rounded-sm bg-[#4e3482] font-semibold text-white my-2 cursor-pointer">
+            Register
+          </button>
+          <h3>
+            Already have an account? Please
+            <Link to="/login" className="text-vsecondary hover:underline">
+              <span> </span> Login
+            </Link>
+          </h3>
+        </form>
+      </div>
     </div>
   );
 };
